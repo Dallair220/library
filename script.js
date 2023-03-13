1b84e747b98ef92a3a027c298473c2f2dde57b53
Dallair220 <93786532+Dallair220@users.noreply.github.com>
2023-03-13 17:57:01 +0100

    Finish project

--- script.js
+++ script.js
@@ -1,26 +1,110 @@
 const myLibrary = [];
+const newBookBtn = document.querySelector('.new');
+const popup = document.querySelector('.popup');
+let tbody = document.createElement('tbody');
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.switchReadStatus = function () {
+  this.read = !this.read;
+};
+
+function resetTable() {
+  tbody.remove();
+  tbody = document.createElement('tbody');
+  const table = document.querySelector('table');
+  table.appendChild(tbody);
+}
+
+function output() {
+  resetTable();
+
+  myLibrary.forEach((bk, index) => {
+    const book = bk;
+
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
+
+    let td = document.createElement('td');
+    td.textContent = book.title;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.textContent = book.author;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.textContent = book.pages;
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    const btn = document.createElement('button');
+    btn.classList.add('read');
+    btn.textContent = book.read ? '✅' : '❌';
+    tr.appendChild(td);
+    td.appendChild(btn);
+    // Add functionality to change read status
+    btn.addEventListener('click', () => {
+      book.switchReadStatus();
+      output();
+    });
+
+    td = document.createElement('td');
+    const btn2 = document.createElement('button');
+    btn2.classList.add('delete');
+    btn2.textContent = 'Delete';
+    tr.appendChild(td);
+    td.appendChild(btn2);
+    // Add functionality to delete row
+    btn2.addEventListener('click', () => {
+      myLibrary.splice(index, !index ? 1 : index);
+      output();
+    });
+  });
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  output();
 }
 
-function output() {
-  // do something
-}
+// popup
+newBookBtn.addEventListener('click', () => {
+  popup.style.display = 'block';
+});
 
-const b1 = new Book('ok', 'We did it!', 245);
-addBookToLibrary(b1);
-const b2 = new Book('Jones', 'Nice!', 234);
+const submitBook = document.querySelector('.sub');
+submitBook.addEventListener('click', (event) => {
+  popup.style.display = 'none';
+  event.preventDefault();
+  // Create new book with form input
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const pages = document.getElementById('pages');
+  const read = document.getElementById('read');
+
+  const addBook = new Book(
+    title.value,
+    author.value,
+    pages.value,
+    read.checked
+  );
+  addBookToLibrary(addBook);
+});
+
+document.addEventListener('click', (event) => {
+  if (!popup.contains(event.target) && !newBookBtn.contains(event.target))
+    popup.style.display = 'none';
+});
+
+const b2 = new Book('Die Kölner Chronik', 'Hermann Weinsberg', 4711, true);
 addBookToLibrary(b2);
-const b3 = new Book('Dies', 'Mice!', 54);
+const b1 = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 208, false);
+addBookToLibrary(b1);
+const b3 = new Book('Die Blechtrommel', 'Günter Grass', 730, true);
 addBookToLibrary(b3);
-const b4 = new Book('Das', 'Hot!', 3457);
-addBookToLibrary(b4);
-
-console.log(myLibrary);
